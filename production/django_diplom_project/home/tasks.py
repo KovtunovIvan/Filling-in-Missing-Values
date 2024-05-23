bbe33eb4f1cb446104d7dd55986c876d9dba0271
kovtunovtn@gmail.com <kovtunovtn@gmail.com>
2024-05-23 17:22:15 +0300

    Fixed the naming mechanism of processed files

--- production/django_diplom_project/home/tasks.py
+++ production/django_diplom_project/home/tasks.py
@@ -49,10 +49,28 @@ def process_large_data(project_id, method_fill_id, method_scaling_id):
         CatBoostRegressor_imputer,
     ]
 
+    all_methods_names = [
+        "mean_fill",
+        "median_fill",
+        "min_fill",
+        "max_fill",
+        "interpolate_fill",
+        "linreg_imputer",
+        "KNN_fill",
+        "DecisionTree_imputer",
+        "RandomForest_imputer",
+        "SVM_imputer",
+        "XGBRegressor_imputer",
+        "CatBoostRegressor_imputer",
+    ]
+
     scaling_methods_names = {
         "standart": "standart_scaling",
         "normalize": "normalization_scaling",
     }
+
+    fill_method_name = all_methods_names[int(method_fill_id)]
+    scaling_method_name = scaling_methods_names[method_scaling_id]
 
     filled_df = all_methods[int(method_fill_id)](df_encoding)
 
@@ -69,14 +87,14 @@ def process_large_data(project_id, method_fill_id, method_scaling_id):
 
     buffer.write(result_df.encode())
 
-    project.processed_csv_file.save(
-        f"{title}_{all_methods[method_fill_id]}.csv", buffer
-    )
+    # Формируем имя файла на основе названия проекта, метода заполнения и метода масштабирования
+    file_name = f"{title}_{fill_method_name}_{scaling_method_name}.csv"
+    project.processed_csv_file.save(file_name, buffer)
 
+    # Также сохраняем файл на диск, чтобы был доступен непосредственно
     processed_file_path = project.processed_csv_file.path
-    with open(processed_file_path, "w") as file:
-        file.write(result_df)
 
+    # Обновляем информацию о задаче в проекте
     project.task_id = process_large_data.request.id
     project.save()
 
